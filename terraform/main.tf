3fbf8745391c9605195e51a7fdc0921a3d05c3e0
Bawantha Rathnayake <Bawantharathnayake395@gmail.com>
2026-06-09 15:00:21 +0530

    update sql working as free tier aws

--- terraform/main.tf
+++ terraform/main.tf
@@ -207,41 +207,65 @@ resource "aws_db_subnet_group" "main" {
 }
 
 # RDS Instance
+# resource "aws_db_instance" "main" {
+#   identifier = "${local.name_prefix}-mysql"
+#
+#   engine         = "mysql"
+#   engine_version = "8.0"
+#   instance_class = var.db_instance_class
+#
+#   allocated_storage     = 20
+#   max_allocated_storage = 20
+#   storage_type          = "gp2"
+#   storage_encrypted     = true
+#
+#   db_name  = "game_db"
+#   username = var.db_username
+#   password = var.db_password
+#
+#   vpc_security_group_ids = [aws_security_group.rds.id]
+#   db_subnet_group_name   = aws_db_subnet_group.main.name
+#
+#   backup_retention_period = 0
+#   backup_window          = "03:00-04:00"
+#   maintenance_window     = "sun:04:00-sun:05:00"
+#
+#   #skip_final_snapshot = var.environment != "prod"
+#   #deletion_protection = var.environment == "prod"
+#
+#   skip_final_snapshot = true
+#   deletion_protection = false
+#
+#   tags = merge(local.common_tags, {
+#     Name = "${local.name_prefix}-mysql"
+#   })
+# }
+
+# Application Load Balancer
+
 resource "aws_db_instance" "main" {
-  identifier = "${local.name_prefix}-mysql"
+  identifier = "sample-game-app-mysql"
 
   engine         = "mysql"
   engine_version = "8.0"
-  instance_class = var.db_instance_class
 
-  allocated_storage     = 20
-  max_allocated_storage = 100
-  storage_type          = "gp2"
-  storage_encrypted     = true
+  instance_class = "db.t3.micro"
+
+  allocated_storage = 20
+  storage_type      = "gp2"
 
   db_name  = "game_db"
   username = var.db_username
   password = var.db_password
 
-  vpc_security_group_ids = [aws_security_group.rds.id]
-  db_subnet_group_name   = aws_db_subnet_group.main.name
+  skip_final_snapshot = true
 
   backup_retention_period = 0
-  backup_window          = "03:00-04:00"
-  maintenance_window     = "sun:04:00-sun:05:00"
 
-  #skip_final_snapshot = var.environment != "prod"
-  #deletion_protection = var.environment == "prod"
-
-  skip_final_snapshot = true
-  deletion_protection = false
-
-  tags = merge(local.common_tags, {
-    Name = "${local.name_prefix}-mysql"
-  })
+  vpc_security_group_ids = [aws_security_group.rds.id]
+  db_subnet_group_name   = aws_db_subnet_group.main.name
 }
 
-# Application Load Balancer
 resource "aws_lb" "main" {
   name               = "${local.name_prefix}-alb"
   internal           = false
