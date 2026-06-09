fe6441688810a223507dfb0abff635bbd122136d
Bawantha Rathnayake <Bawantharathnayake395@gmail.com>
2026-06-09 14:39:24 +0530

    sql details update again

--- terraform/main.tf
+++ terraform/main.tf
@@ -226,7 +226,7 @@ resource "aws_db_instance" "main" {
   vpc_security_group_ids = [aws_security_group.rds.id]
   db_subnet_group_name   = aws_db_subnet_group.main.name
 
-  backup_retention_period = 1
+  backup_retention_period = 0
   backup_window          = "03:00-04:00"
   maintenance_window     = "sun:04:00-sun:05:00"
 
