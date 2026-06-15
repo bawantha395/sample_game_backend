20baa9c505090c7938568e1cd961998bd25bdbf0
Bawantha Rathnayake <Bawantharathnayake395@gmail.com>
2026-06-15 10:12:20 +0530

    chore: downgrade database instance class to db.t2.micro

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -38,7 +38,7 @@ module "registry" {
 module "database" {
   source                        = "../../modules/aws/database"
   environment                   = var.environment
-  app_name                      = var.app_name
+  db_instance_class             = "db.t2.micro"
   private_subnet_ids            = module.foundation.private_subnet_ids
   vpc_id                        = module.foundation.vpc_id
   db_username                   = var.db_username
