4336484558e7069846ead3cdf816c024a52248ce
Bawantha Rathnayake <Bawantharathnayake395@gmail.com>
2026-06-15 09:56:25 +0530

    variables

--- terraform/modules/aws/database/variables.tf
+++ terraform/modules/aws/database/variables.tf
@@ -5,4 +5,7 @@ variable "eks_cluster_security_group_id" { type = string }
 variable "db_instance_class" { type = string }
 variable "db_username" { type = string }
 variable "db_password" { type = string }
-variable "tags" { type = map(string) default = {} }
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
--- terraform/modules/aws/registry/variables.tf
+++ terraform/modules/aws/registry/variables.tf
@@ -1,3 +1,6 @@
 variable "environment" { type = string }
 variable "app_name" { type = string }
-variable "tags" { type = map(string) default = {} }
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
