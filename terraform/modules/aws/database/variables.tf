766040e54ff0e750005405a4d02f4601854ba7ad
Bawantha Rathnayake <Bawantharathnayake395@gmail.com>
2026-06-25 11:31:10 +0530

    fixing rdss

--- terraform/modules/aws/database/variables.tf
+++ terraform/modules/aws/database/variables.tf
@@ -1,11 +1,47 @@
-variable "environment" { type = string }
-variable "vpc_id" { type = string }
-variable "private_subnet_ids" { type = list(string) }
-variable "eks_cluster_security_group_id" { type = string }
-variable "db_instance_class" { type = string }
-variable "db_username" { type = string }
-variable "db_password" { type = string }
+variable "environment" {
+  type        = string
+  description = "The environment name (e.g., dev, staging, prod)"
+
+  validation {
+    condition     = contains(["dev", "staging", "prod"], var.environment)
+    error_message = "The environment must be one of: dev, staging, prod."
+  }
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "The VPC ID where the RDS instance will be deployed"
+}
+
+variable "private_subnet_ids" {
+  type        = list(string)
+  description = "List of private subnet IDs for the DB subnet group"
+}
+
+variable "eks_cluster_security_group_id" {
+  type        = string
+  description = "The security group ID of the EKS cluster to allow traffic from"
+}
+
+variable "db_instance_class" {
+  type        = string
+  description = "The instance type of the RDS instance"
+  default     = "db.t3.micro"
+}
+
+variable "db_username" {
+  type        = string
+  description = "The master username for the database"
+}
+
+variable "db_password" {
+  type        = string
+  description = "The master password for the database"
+  sensitive   = true
+}
+
 variable "tags" {
-  type    = map(string)
-  default = {}
+  type        = map(string)
+  description = "A mapping of tags to assign to all resources"
+  default     = {}
 }
