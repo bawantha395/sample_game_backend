523f7fad64cae494de982346249f462168d86ae0
Bawantha Rathnayake <Bawantharathnayake395@gmail.com>
2026-06-26 10:04:09 +0530

    micro servicessss

--- terraform/modules/aws/registry/outputs.tf
+++ terraform/modules/aws/registry/outputs.tf
@@ -1,3 +1,8 @@
+# ==============================================================================
+# FIXED: ITERATING OVER MULTIPLE ECR REPOSITORY URLS
+# ==============================================================================
+
 output "repository_urls" {
-  value = { for k, v in aws_ecr_repository.main : k => v.repository_url }
+  description = "Map of ECR repository names to their respective registry URLs"
+  value       = { for k, v in aws_ecr_repository.main : k => v.repository_url }
 }
